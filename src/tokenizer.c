ecc7e4e2d639f222922620a1dc6ea4adfed0c2c1
student <hasimental@miners.utep.edu>
2021-03-01 00:04:35 -0700

    finished the remaining tokenizer functions that were needed just need to piece together

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,30 +1,120 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
 
+// here we will check to see if it is a space char such as a space tab or end of line
 int space_char (char c){
-  if(c == '\t' || c == ' ' && c != '\0'){
+  if((c == '\t' || c == ' ') && c != '\0'){
     return 1;
   }
   else
   return 0;
 }
 
+//here we will check to see if it is a non space character such as a anything other than a tab,space, or end of line
 int non_space_char(char c){
-  if(c != '\t' || c != ' ' && c != '\0')
+  if((c != '\t' || c != ' ') && c != '\0')
     return 1;
   else
     return 0;
 }
 
-char *word_start(char *s){
- 
+//here we will check to see where the start of the word is
+char  *word_start(char *str){
+
+  //iterate through the string as long as there is a space character
+    while(*str){
+      if(non_space_char(*str)){
+	return str;
+      }
+      str++;
+  }
+    //if found return string
+  return '\0';
 }
 
+// here we will check to see if there is a word terminator 
 char *word_terminator(char *word){
-
+  // iterate through checking if it is a non space character
+  while(*word){
+    if(non_space_char(*word)){
+      return word;
+    }
+    word++;
+  }
+  // if it is a space character then return 
+  return '\0';
 }
 
-int count_words(char *s){
+int count_words(char *str){
   int count = 0;
-
+  
+  while(*str){
+    if(space_char(*str)){
+      count++;
+    }
+    str++;
+  }
   return count;
 }
+
+/* Returns a fresly allocated new zero-terminated string 
+   containing <len> chars from <inStr> */
+char *copy_str(char *inStr, short len){
+  int i;
+  char *copy_of_string = malloc (sizeof(char) * (len+1));
+
+  for(int i = 0; i < len; i++){
+    copy_of_string[i] = inStr[i];
+  }
+  //this is to set to null
+  copy_of_string[i] ='\0';
+  
+  return copy_of_string;
+}
+
+/* Returns a freshly allocated zero-terminated vector of freshly allocated 
+   space-separated tokens from zero-terminated str.
+ 
+   For example, tokenize("hello world string") would result in:
+     tokens[0] = "hello"
+     tokens[1] = "world"
+     tokens[2] = "string" 
+     tokens[3] = 0
+*/
+char **tokenize(char *str){
+  int i= 0;
+  int totalWords = count_words(str);
+  char **pointer = malloc(sizeof(char*) * (totalWords +1));
+  char *startOfWord = str;
+
+  for(int i = 0; i < totalWords; i++){
+    int length = word_terminator(str) - word_start(str);
+    pointer[i] = copy_str(str,length);
+
+    str = word_terminator(str);
+    str = word_start(str);
+  }
+  pointer[i] = 0;
+  return pointer;
+}
+
+/* Prints all tokens. */
+void print_tokens(char **tokens){
+  int i;
+  for(int i = 0; tokens[i] != NULL; i++){
+    printf("%s\n", tokens[i]);
+  }
+}
+
+/* Frees all tokens and the vector containing themx. */
+void free_tokens(char **tokens){
+  int i;
+  //free all the words that are in tokens
+  for(int i = 0; tokens[i] != 0; i++){
+    free(tokens[i]);
+  }
+  // free the memory for tokens
+  free(tokens);
+}
+
--- src/uimain.c
+++ src/uimain.c
@@ -1,39 +1,16 @@
 #include <stdio.h>
 #include "tokenizer.h"
 
+
 int main(){
-  
+  //here we are setting the max string
+  int max_str =1000; 
+ char input_string[max_str];
    puts("Welcome!");
 
-     while(1){
-    fputs("Please enter t to tokenize and q to quit");
-    fflush(stdout);
-
-      int c;
-      while((c == getchar() == '\n'))
-	// this will check if it is an end of file and just jump to END
-	if(c == EOF)
-	  goto END;
-      // here are cases where we will check to see if it is 
-      switch(c){
-	// if user inputs t then it will ask for the users string that they want 
-      case 't' :
-	puts("Please enter the string that you would like tokenized: ");
-	break;
-	// here is where we will terminate if user inputs q
-      case 'q':
-	puts("Thank you for using Tokenizer!");
-	goto END;
-	
-      case '\n':
-	break;
-      default:
-	printf("'%c' not a valid option please enter a valid option: \n ", c);
-	
-      }
-    }
-      // this is where the program jumps to if user inputs q and ends program
-    END:
-      return 0;
-   
+   // this will ask for the user to input the string they would like tokenized
+   puts("Please enter the string that you would like tokenized: ");
+   fgets(input_string, max_str, stdin);
+   //here for testing
+   non_space_char(input_string);
  }
